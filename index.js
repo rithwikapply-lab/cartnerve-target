22d583526e2133f6cf6861f786874a27e4bdfc73
rithwikapply-lab <rithwik.apply@gmail.com>
2026-04-05 08:00:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { CartNerveAgent } = require('cartnerve-agent')
 const agent = new CartNerveAgent({
   apiKey: "cn_d5656fe6bdd83df832076f9c7dcfdd480f560fd969c00b49",
   projectId: "a46fcc94-c88d-45cb-93d3-c451b790c633",
-  baseUrl: "https://cartnerve.com",
   services: [
     { 
       name: "cartnerve-target", 
